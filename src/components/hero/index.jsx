a72c6143e1689857297e03c69a6166aea0e1f044
naveenkumar-dev13 <naveenkumar.mfai@gmail.com>
2025-08-07 20:14:01 +0530

    Update Hero component styles for improved layout and responsiveness

--- src/components/hero/index.jsx
+++ src/components/hero/index.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 const Hero = () => {
   return (
-    <div className=" w-full bg-[#FBFBFB]   rounded-lg h-[70vh] !my-6  flex items-center relative ">
+    <div className=" w-full bg-red-50  rounded-lg h-[70vh] !my-6  flex items-center relative ">
       <div className="flex flex-col  justify-center gap-3 !pl-10">
         <p className="text-sm">
           Order Restaurant food, takeaway and groceries.
@@ -15,7 +15,7 @@ const Hero = () => {
           <p>Enter a postcode to see what we deliver</p>
         </div>
       </div>
-      <div className="relative   top-0 right-30 w-[90%] h-[100%] z-20">
+      <div className="relative   top-6 right-50 w-[50%] h-[100%] z-20">
         <Image
           src={hero_1}
           alt="hero"
@@ -23,7 +23,7 @@ const Hero = () => {
         />
       </div>
       <div className="bg-[#fc8a06] w-[500px] h-[90%]   rounded-l-full  absolute bottom-0  right-0 z-0">
-        <div className=" w-1/2 bg-[#F8F8F8]/80  rounded-lg  relative  top-10 left-32   !p-2 ">
+        <div className=" w-1/2 bg-[#F8F8F8]/80  rounded-lg  relative  top-23 left-32 z-20  !p-2 ">
           <div className="flex justify-between items-center !mb-2">
             <div className="relative h-[20px] w-[50px]">
               <Image src={Logo} alt="logo" fill className="object-contain" />
@@ -35,7 +35,7 @@ const Hero = () => {
             <p>Awaiting Restaurant acceptance </p>
           </div>
         </div>
-        <div className=" w-1/2 bg-[#F8F8F8]/80  rounded-lg  relative  top-30 left-56   !p-2 ">
+        <div className=" w-1/2 bg-[#F8F8F8]/80  rounded-lg  relative  top-38 left-52 z-20  !p-2 ">
           <div className="flex justify-between items-center !mb-2">
             <div className="relative h-[20px] w-[50px]">
               <Image src={Logo} alt="logo" fill className="object-contain" />
@@ -47,7 +47,7 @@ const Hero = () => {
             <p>Awaiting Restaurant acceptance </p>
           </div>
         </div>
-        <div className=" w-1/2 bg-[#F8F8F8]/80  rounded-lg  relative  top-50 left-32   !p-2 ">
+        <div className=" w-1/2 bg-[#F8F8F8]/80  rounded-lg  relative  top-50 left-32 z-20  !p-2 ">
           <div className="flex justify-between items-center !mb-2">
             <div className="relative h-[20px] w-[50px]">
               <Image src={Logo} alt="logo" fill className="object-contain" />
@@ -59,12 +59,8 @@ const Hero = () => {
             <p>Awaiting Restaurant acceptance </p>
           </div>
         </div>
-        <div className="relative   bottom-50 right-0 w-[70%] h-[100%]  ">
-          <Image
-            src={hero_2}
-            alt="hero"
-            className="object-contain w-full h-full   "
-          />
+        <div className="relative   bottom-26.5 right-20 w-[80%] -z-0  ">
+          <Image src={hero_2} alt="hero" className="   h-[350px]   " />
         </div>
       </div>
     </div>
